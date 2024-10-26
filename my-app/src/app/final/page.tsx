1c49a9cd0d7e59901f27145e7ebf6ef036e5c484
Theo Furtado Mauricio <58632127+theofurtado05@users.noreply.github.com>
2024-10-26 10:20:11 -0300

    fix: build

--- my-app/src/app/final/page.tsx
+++ my-app/src/app/final/page.tsx
@@ -47,55 +47,61 @@ export default function DiagnosisResultScreen() {
 
   //@ts-ignore
   const getMaturityLevel = (points) => {
-    if (points >= 0 && points <= 10) {
-      localStorage.setItem('video', "https://www.youtube.com/embed/11KwSkjwAyI?si=Nw6294PAqUl1cjgR")
-      return {
-        level: 1,
-        title: "Inicial",
-        link: "https://www.youtube.com/embed/11KwSkjwAyI?si=Nw6294PAqUl1cjgR",
-        description: "Você está operando em um ambiente onde os processos são informais, imprevisíveis e, às vezes, até caóticos, devido sua instabilidade. O sucesso da sua organização depende muito da competência e heroísmo dos funcionários, e pode parecer que você está sempre 'apagando incêndios'. Não há uma padronização clara ou previsibilidade nos resultados. A comunicação entre áreas não é tão eficaz, e a gestão dos processos acaba sendo mais reativa. Pode ser frustrante, mas é importante reconhecer que sem uma base sólida, o controle e a estabilidade nos processos são difíceis de alcançar."
-
-      }
-    } else if (points >= 11 && points <= 15) {
-      localStorage.setItem('video', "https://www.youtube.com/embed/ERSUPHFbGU4?si=BOBgLzt5vSdlnf3s")
-      return {
-        level: 2,
-        title: "Processo em desenvolvimento",
-        link: "https://www.youtube.com/embed/ERSUPHFbGU4?si=BOBgLzt5vSdlnf3s",
-        description: "Você está começando a construir uma base mais estável para os seus processos. Aqui, você já começa a identificar os principais processos e repetir práticas de sucesso de projetos anteriores em aplicações similares. Destaca-se que ele pode não ser permitido para todos os projetos da organização. Os gerentes começam a ter ferramentas para planejar e controlar os custos, prazos e escopo, e você começa a ver acordos de projetos e responsabilidades sendo estruturados. Ainda assim, o sucesso muitas vezes depende do trabalho individual."        
-      }
-    } else if (points >= 16 && points <= 20) {
-      localStorage.setItem('video', "https://www.youtube.com/embed/oTlVrXg6DCs?si=gnjbVDHot0HHQBTL")
-      return {
-        level: 3,
-        title: "Médio",
-        link: "https://www.youtube.com/embed/oTlVrXg6DCs?si=gnjbVDHot0HHQBTL",
-        description: "Você chegou a um ponto em que os processos são mais consistentes, padronizados e documentados, de forma a fornecer orientação entre os projetos. Agora, sua organização opera com um conjunto integrado de práticas, onde os processos não pertencem mais aos projetos, e sim à empresa. As equipes trabalham de forma mais colaborativa, e o treinamento é planejado conforme as necessidades de cada perfil. Isso traz mais segurança e eficiência para as operações, mas lembre-se de que ainda há caminhos a serem explorados."
-      }
-    } else if (points >= 21 && points <= 25) {
-      localStorage.setItem('video', "https://www.youtube.com/embed/WeGvfZtPdug?si=0-reb1Dm0UdBWcKL")
-      return {
-        level: 4,
-        title: "Gerenciado",
-        link: "https://www.youtube.com/embed/WeGvfZtPdug?si=0-reb1Dm0UdBWcKL",
-        description: "Você alcançou um estágio onde os processos são bem estáveis e conhecidos por todos. A organização agora pode gerar indicadores para avaliar e monitorar o desempenho e a qualidade dos processos. Com o envolvimento forte das equipes e a alocação correta de recursos, será possível gerenciar os processos com mais precisão, de forma quantitativa a partir das métricas. Ainda assim, o foco deve ser em continuar melhorando e ajustando conforme as análises de desempenho mostram os caminhos a seguir."
-      }
-    } else if (points >= 26 && points <= 30) {
-      localStorage.setItem('video', "https://www.youtube.com/embed/sxguufzRJXE?si=FiAH3RerLCVruz7X")
-      return {
-        level: 5,
-        title: "Otimizado",
-        link: "https://www.youtube.com/embed/sxguufzRJXE?si=FiAH3RerLCVruz7X",
-        description: "Parabéns, você chegou ao topo! Sua organização agora foca em melhorias contínuas, aprendendo com cada projeto e utilizando indicadores para refinar processos. As inovações tecnológicas são introduzidas para otimizar ainda mais as operações. A cultura de melhoria contínua está enraizada, e todos os processos são sistematicamente aprimorados, de forma que sejam flexíveis e capazes de responder às oportunidades e mudanças. Parabéns, você faz parte de uma organização que atingiu o mais alto nível de maturidade em processos, com uma infraestrutura consolidada que apoia o desenvolvimento e a mudança constante."
+    if (typeof window !== "undefined") {
+      if (points >= 0 && points <= 10) {
+        localStorage.setItem('video', "https://www.youtube.com/embed/11KwSkjwAyI?si=Nw6294PAqUl1cjgR")
+        return {
+          level: 1,
+          title: "Inicial",
+          link: "https://www.youtube.com/embed/11KwSkjwAyI?si=Nw6294PAqUl1cjgR",
+          description: "Você está operando em um ambiente onde os processos são informais, imprevisíveis e, às vezes, até caóticos, devido sua instabilidade. O sucesso da sua organização depende muito da competência e heroísmo dos funcionários, e pode parecer que você está sempre 'apagando incêndios'. Não há uma padronização clara ou previsibilidade nos resultados. A comunicação entre áreas não é tão eficaz, e a gestão dos processos acaba sendo mais reativa. Pode ser frustrante, mas é importante reconhecer que sem uma base sólida, o controle e a estabilidade nos processos são difíceis de alcançar."
+        }
+      } else if (points >= 11 && points <= 15) {
+        localStorage.setItem('video', "https://www.youtube.com/embed/ERSUPHFbGU4?si=BOBgLzt5vSdlnf3s")
+        return {
+          level: 2,
+          title: "Processo em desenvolvimento",
+          link: "https://www.youtube.com/embed/ERSUPHFbGU4?si=BOBgLzt5vSdlnf3s",
+          description: "Você está começando a construir uma base mais estável para os seus processos. Aqui, você já começa a identificar os principais processos e repetir práticas de sucesso de projetos anteriores em aplicações similares. Destaca-se que ele pode não ser permitido para todos os projetos da organização. Os gerentes começam a ter ferramentas para planejar e controlar os custos, prazos e escopo, e você começa a ver acordos de projetos e responsabilidades sendo estruturados. Ainda assim, o sucesso muitas vezes depende do trabalho individual."        
+        }
+      } else if (points >= 16 && points <= 20) {
+        localStorage.setItem('video', "https://www.youtube.com/embed/oTlVrXg6DCs?si=gnjbVDHot0HHQBTL")
+        return {
+          level: 3,
+          title: "Médio",
+          link: "https://www.youtube.com/embed/oTlVrXg6DCs?si=gnjbVDHot0HHQBTL",
+          description: "Você chegou a um ponto em que os processos são mais consistentes, padronizados e documentados, de forma a fornecer orientação entre os projetos. Agora, sua organização opera com um conjunto integrado de práticas, onde os processos não pertencem mais aos projetos, e sim à empresa. As equipes trabalham de forma mais colaborativa, e o treinamento é planejado conforme as necessidades de cada perfil. Isso traz mais segurança e eficiência para as operações, mas lembre-se de que ainda há caminhos a serem explorados."
+        }
+      } else if (points >= 21 && points <= 25) {
+        localStorage.setItem('video', "https://www.youtube.com/embed/WeGvfZtPdug?si=0-reb1Dm0UdBWcKL")
+        return {
+          level: 4,
+          title: "Gerenciado",
+          link: "https://www.youtube.com/embed/WeGvfZtPdug?si=0-reb1Dm0UdBWcKL",
+          description: "Você alcançou um estágio onde os processos são bem estáveis e conhecidos por todos. A organização agora pode gerar indicadores para avaliar e monitorar o desempenho e a qualidade dos processos. Com o envolvimento forte das equipes e a alocação correta de recursos, será possível gerenciar os processos com mais precisão, de forma quantitativa a partir das métricas. Ainda assim, o foco deve ser em continuar melhorando e ajustando conforme as análises de desempenho mostram os caminhos a seguir."
+        }
+      } else if (points >= 26 && points <= 30) {
+        localStorage.setItem('video', "https://www.youtube.com/embed/sxguufzRJXE?si=FiAH3RerLCVruz7X")
+        return {
+          level: 5,
+          title: "Otimizado",
+          link: "https://www.youtube.com/embed/sxguufzRJXE?si=FiAH3RerLCVruz7X",
+          description: "Parabéns, você chegou ao topo! Sua organização agora foca em melhorias contínuas, aprendendo com cada projeto e utilizando indicadores para refinar processos. As inovações tecnológicas são introduzidas para otimizar ainda mais as operações. A cultura de melhoria contínua está enraizada, e todos os processos são sistematicamente aprimorados, de forma que sejam flexíveis e capazes de responder às oportunidades e mudanças. Parabéns, você faz parte de uma organização que atingiu o mais alto nível de maturidade em processos, com uma infraestrutura consolidada que apoia o desenvolvimento e a mudança constante."
+        }
+      } else {
+        localStorage.setItem('video', "https://www.youtube.com/embed/11KwSkjwAyI?si=Nw6294PAqUl1cjgR")
+        return {
+          level: 0,
+          title: "Calculando...",
+          link: "https://www.youtube.com/embed/11KwSkjwAyI?si=Nw6294PAqUl1cjgR",
+          description: "Estamos calculando seu resultado, por favor aguarde..."
+        }
       }
     } else {
-      if (typeof window !== "undefined") {
-        localStorage.setItem('video', "https://www.youtube.com/embed/11KwSkjwAyI?si=Nw6294PAqUl1cjgR")
-      }
       return {
         level: 0,
         title: "Calculando...",
-        link: "https://www.youtube.com/embed/11KwSkjwAyI?si=Nw6294PAqUl1cjgR",
+        link: "",
         description: "Estamos calculando seu resultado, por favor aguarde..."
       }
     }
